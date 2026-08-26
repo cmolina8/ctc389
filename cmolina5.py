5ebdd3e79c66f887c135432ff0cbb2ad4dfeb382
cmolina8 <molics@fusd.net>
2026-08-26 22:36:53 +0000

    lab #5

---
+++ cmolina5.py
@@ -0,0 +1,41 @@
+#Ceiry Molina 
+#Lab 5
+
+word = "No loop"
+
+n1= 20
+n2=22
+n3=24
+n4=26
+n5=28
+n6=30
+n7=32
+n8=34 
+n9=36
+n10=38
+
+print(word)
+print(n1)
+print(n2)
+print(n3)
+print(n4)
+print(n5)
+print(n6)
+print(n7)
+print(n8)
+print(n9)
+print(n10)
+
+nums = ["With loop",20,22,24,26,28,30,32,34,36,38]
+
+for i in nums:
+    print(i)
+
+words = ["mangos","grapes", "green beans", "lettuce", "cucumber"]
+print(words[0])
+print(words[1])
+print(words[2])
+print(words[3])
+print(words[4])
+
+
