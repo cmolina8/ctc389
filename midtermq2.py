830501c34ba7d36219ea307b2328517c159771b1
cmolina8 <molics@fusd.net>
2026-09-10 02:29:03 +0000

    midtermq2.py

---
+++ midtermq2.py
@@ -0,0 +1,15 @@
+#Ceiry Molina 
+#Midterm Q2
+
+def area_rectangle(b,h):
+    area = b*h
+    return area
+
+base = int(input("What is the base of the rectangle loser?"))
+
+height = int(input("What is the height of the rectangle loser?"))
+
+x = area_rectangle(base,height)
+
+print("The area of the rectangle is",x,"loser")
+
