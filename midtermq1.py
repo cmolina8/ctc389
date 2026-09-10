4e85f34d1e07f19f2338045aa7b7b716e2a11103
cmolina8 <molics@fusd.net>
2026-09-10 02:26:04 +0000

    midtermq1.py

---
+++ midtermq1.py
@@ -0,0 +1,13 @@
+#Ceiry Molina 
+#Midterm Q1
+
+name = input("Yo, what is you name loser?")
+
+number = int(input("Enter a number please"))
+
+numberadd = int(input("How much would you like to add to the number loser?"))
+
+print(name, "you entered",number, "and added", numberadd,"to get a total of", number+numberadd)
+
+
+
